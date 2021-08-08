acdc2e02c40f5d34a79d0135218a8bc85377d72f
Andrew Bounds <andybounds@gmail.com>
2021-08-08 14:05:10 -0400

    Added mission generator tool

---
+++ Tools/MissionGenerator/MissionGenerator/CompDetails.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiveOhFirstMissionFileGenerator
+{
+    public class CompDetails
+    {
+        public string Title { get; set; } = "";
+        public string Version { get; set; } = "";
+        public string Author { get; set; } = "";
+        public string EditorVersion { get; set; } = "";
+        public List<string> RequiredAddons { get; set; } = new();
+        public List<string> RawObjectData { get; set; } = new();
+        public List<string> Connections { get; set; } = new();
+    }
+}
