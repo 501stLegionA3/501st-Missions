53ebd5deaa18221cc928ce3d9a399ccc15976ff7
Soyvolon <Soyvolon@users.noreply.github.com>
2022-01-08 14:53:44 +0000

    Mission File Release

---
+++ Missions/Live/501st_Legion_FunOp_Venator_V1-7-3-beta_%28NO_MCC%29%3A_Ruha_.ruha/CustomControlClasses.hh
@@ -0,0 +1,6 @@
+//Exported via Arma Dialog Creator (https://github.com/kayler-renslow/arma-dialog-creator)
+#ifndef HG_CustomControlClasseshh
+#define HG_CustomControlClasseshh 1
+//Create a header guard to prevent duplicate include.
+
+#endif
